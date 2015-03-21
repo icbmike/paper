fa9a161a57c0fe99fe13d6d6a9d3dc710826fbac
Michael Little <bio595@gmail.com>
2015-03-21 17:48:58 +1100

    Create network component

---
+++ src/js/components/network/index.js
@@ -0,0 +1,4 @@
+
+var app = require('angular').module('paperApp');
+
+app.directive('networkDirective', require('./network-directive'));
---
+++ src/js/components/network/network-directive.js
@@ -0,0 +1,13 @@
+
+
+module.exports = function(){
+	return {
+		scope: {},
+		controller: function(){
+			
+		},
+		controllerAs: 'networkCntrl',
+		bindToController: true,
+		template: require('./network-template.html').trim()
+	};
+};
